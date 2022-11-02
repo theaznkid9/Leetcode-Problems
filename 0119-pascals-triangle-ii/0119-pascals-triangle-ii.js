8bdaa352197b3c0a96ebab0fe94953c9c140e435
Alexander Lee <alexylee15@gmail.com>
2022-11-02 15:38:32 -0400

    Time: 84 ms (74.33%), Space: 41.7 MB (89.00%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.js
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.js
@@ -3,31 +3,15 @@
  * @return {number[]}
  */
 var getRow = function(rowIndex) {
-    if (rowIndex === 0) {
-        return [1];
-    }
-    if (rowIndex === 1) {
-        return [1, 1];
-    }
-    let currentRow = 2;
-    let currentArray = [1, 2, 1];
-    const helper = (array) => {
-        if (currentRow === rowIndex) {
-            return;
+    var row = [1];
+    
+    for(var i = 1 ; i <= rowIndex ; i++) {
+        for(var j = i; j > 0; j--) {
+            if(j === i)
+                row[j] = 1;
+            else
+                row[j] = row[j - 1] + row[j];
         }
-        let newArray = [1];
-        for (let i = 1; i < array.length; i++) {
-            if (i === array.length - 1) {
-                newArray.push(array[i] + array[i - 1]);
-                newArray.push(1);
-            } else {
-                newArray.push(array[i] + array[i - 1]);
-            }
-        }
-        currentArray = newArray;
-        currentRow++;
-        helper(currentArray);
     }
-    helper(currentArray);
-    return currentArray;
+    return row;
 };

f4882ce0b3d976f0d85a23a41137d3be8addc401
Alexander Lee <alexylee15@gmail.com>
2022-11-10 13:13:45 -0500

    Time: 89 ms (91.52%), Space: 50.9 MB (83.15%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+const removeDuplicates = s => {
+  const arr = s.split('');
+  let ans = 0;
+  for (let i = 0; i < arr.length; ++i) {
+    ans === 0 || arr[i] !== arr[ans - 1] ? (arr[ans++] = arr[i]) : --ans;
+  }
+  return arr.slice(0, ans).join('');
+};
